e609e0d0dab6a7080e27d02c1c74697d4b204d31
Agustin Mendez <github@menduz.com>
2021-01-05 20:50:28 -0300

    first working commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { ILoggerComponent } from "@well-known-components/interfaces"
+
+const print = (kind: string, loggerName: string, message: string, extra: any) => {
+  // logs should go to the stderr to be properly forwarded in some environments
+  const extraLine = extra ? "\t" + JSON.stringify(extra) : ""
+  return process.stderr.write(
+    new Date().toISOString() + " [" + kind + "] (" + loggerName + "): " + message + extraLine + "\n"
+  )
+}
+
+/**
+ * Creates a scoped logger component.
+ * @public
+ */
+export function createLogComponent(): ILoggerComponent {
+  return {
+    getLogger(loggerName: string) {
+      return {
+        log(message: string, extra?: Record<string, string | number>) {
+          print("LOG", loggerName, message, extra)
+        },
+        warn(message: string, extra?: Record<string, string | number>) {
+          print("WARNING", loggerName, message, extra)
+        },
+        info(message: string, extra?: Record<string, string | number>) {
+          print("INFO", loggerName, message, extra)
+        },
+        debug(message: string, extra?: Record<string, string | number>) {
+          print("DEBUG", loggerName, message, extra)
+        },
+        error(error: string | Error, extra?: Record<string, string | number>) {
+          print("DEBUG", loggerName, error.toString(), extra || error)
+        },
+      }
+    },
+  }
+}
